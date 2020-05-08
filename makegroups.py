0d9f322b6173ff72216654e28d5543548080f619
Celticcow <gregory.dunlap@gmail.com>
2020-05-08 10:28:33 -0500

    added code to add group with color based on level

--- makegroups.py
+++ makegroups.py
@@ -66,19 +66,77 @@ def importdata(filename):
 """
 make group
 """
-def create_group():
-    pass
+def create_group(appgroup, ip_addr, sid):
+    print("In function create_group()")
+    debug = 1
+
+    if(apifunctions.group_exist(ip_addr, appgroup.get_name(), sid) == False):
+        if(appgroup.get_level() == "Production"):
+            color = "crete blue"
+        else:
+            color = "brown"
+
+        newgrp_json = {
+            "name" : appgroup.get_name(),
+            "color" : color,
+            "comments" : appgroup.get_comment()
+        }
+
+        add_result = apifunctions.api_call(ip_addr, "add-group", newgrp_json, sid)
+
+        print(json.dumps(add_result))
 
 """
 main function
 """
 def main():
+    debug = 1
+
     grpdate = set()
 
     grpdata = importdata('EAI_PGH2LAS.csv') #EAI_PGH2LAS.csv
 
+    ip_addr = input("enter IP of MDS : ")
+    ip_cma  = input("enter IP of CMA : ")
+    user    = input("enter P1 user id : ")
+    password = getpass.getpass('Enter P1 Password : ')
+
+    sid = apifunctions.login(user, password, ip_addr, ip_cma)
+
+    if(debug == 1):
+        print("session id : " + sid)
+    #do mds login here
+
+    publish_counter = 0
+
     for grp in grpdata:
+        print("EXP : ", end='')
         grp.print_appgroup()
+        create_group(grp, ip_addr, sid)
+        publish_counter += 1
+        
+        if(publish_counter == 50):
+            ##call publish
+            print("incremental publish")
+            time.sleep(10)
+            publish_result = apifunctions.api_call(ip_cma, "publish", {}, sid)
+            print("publish results : " + json.dumps(publish_result))
+            time.sleep(20)
+            publish_counter = 0
+    
+    ## Publish pending changes
+    print("Start of Publish ... zzzzz")
+    time.sleep(10)
+    publish_result = apifunctions.api_call(ip_cma, "publish", {}, sid)
+    print("publish results : " + json.dumps(publish_result))
+
+    time.sleep(10)
+
+    ## Logout
+    logout_result = apifunctions.api_call(ip_addr, "logout", {}, sid)
+    if(debug == 1):
+        print(logout_result)
+#end of main()
 
 if __name__ == "__main__":
     main()
